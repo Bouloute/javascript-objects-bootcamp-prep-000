2911065c3f4495b6f81504a0d1906b735e268e1f
Annaick Lewis Dagnino <annaick1995@gmail.com>
2019-11-07 19:27:01 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,7 +2,8 @@ var playlist = {artist: 'song'}
 
 function updatePlaylist(playlistToUpdate, artistName, songTitle){
   
-  var updatedPlaylist = Object.assign({},playlistToUpdate, {[artistName]: songTitle})
+  var updatedPlaylist = Object.assign({}, playlistToUpdate, {[artistName]: songTitle})
+  console.log("playlist : " + playlist + "\nupdatedPlaylist : " + updatedPlaylist )
   
   return updatedPlaylist
 }
